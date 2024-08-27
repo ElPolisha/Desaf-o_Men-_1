a761c307bc4dfb02e58cf2567360978a76af0299
ElPolisha <lautaro.arrigoni@correo.ucu.edu.uy>
2024-08-27 20:16:15 -0300

    Comentario agregado

--- src/Dish.cs
+++ src/Dish.cs
@@ -3,12 +3,15 @@ namespace Ucu.Poo.Restaurant;
 /// <summary>
 /// Represent un plato individual en el menú del restaurant.
 /// </summary>
+
+/// This implements the Dish.
 public class Dish
 {
     public string Name { get; private set; }
     public decimal Price { get; private set ; }
     public bool IsVegetarian { get; private set; }
 
+    
     public Dish(string name, decimal price, bool isVegetarian)
     {
         Name = name;
