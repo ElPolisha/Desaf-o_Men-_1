dd36a22b8ff0a80ff8639c0e96757e1d3920cefd
Agustin <agustincigaran@icloud.com>
2024-08-27 20:17:39 -0300

    Agregue la clase table

--- src/Table.cs
+++ src/Table.cs
@@ -1,3 +1,6 @@
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+
 namespace Ucu.Poo.Restaurant;
 
 using System.Collections;
@@ -7,9 +10,35 @@ using System.Collections;
 /// </summary>
 public class Table
 {
+    
     private ArrayList order = new ArrayList();
     public bool HasOrders()
     {
         return this.order.Count > 0;
+    }
+    public int Number { get; set; }
+    public bool IsOccupied { get; set; }
+    public void Occupy()
+    {
+        if (IsOccupied)
+        {
+            Console.Write("\n La mesa esta ocupada. ");
+        }
+        else
+        {
+            IsOccupied = true;
+            Console.Write("\n La mesa esta libre. ");
+        }
+    }
+    public void Free()
+    {
+        IsOccupied = false;
+        Console.Write("\n La mesa esta libre. ");
+    }
+
+    public void AddToOrder(object dish)
+    {
+        order.Add(dish);
+        Console.WriteLine("\n Se agrego el plato. ");
     }
 }
