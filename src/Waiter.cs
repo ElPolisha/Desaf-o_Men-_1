ca922b7170bebc9dd4f07b9797c2ec9daca0f4b0
bernaochoa <166560678+bernaochoa@users.noreply.github.com>
2024-08-27 21:00:30 -0300

    Paso 4 waiter

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -7,5 +7,21 @@ namespace Ucu.Poo.Restaurant;
 /// </summary>
 public class Waiter
 {
+    public string Name { get; set; }
     private ArrayList assignedTables = new ArrayList();
+
+    public Waiter(string name)
+    {
+        Name = name;
+    }
+    
+    public void AssignTable(Table table)
+    {
+        assignedTables.Add(table);
+    }
+
+    public void TakeOrder(Table table, Dish dish)
+    { 
+        table.AddToOrder(dish);
+    }
 }
